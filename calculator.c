ae24e8c155152c737299554bfada94b15ca8f883
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2022-03-19 21:01:07 +0530

    calculator using switch-case

--- calculator.c
+++ calculator.c
@@ -1,40 +1,37 @@
 // write a programme to create a calculator using switch case //
-//Sanket Banerjee//
+// Sanket Banerjee//
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
   char op;
   double first, second;
-  float result;
   printf("Enter an operator (+, -, *, /): ");
   scanf("%c", &op);
   printf("Enter First Value: ");
   scanf("%lf", &first);
   printf("Enter Second Value: ");
-  scanf("%lf", &second );
+  scanf("%lf", &second);
 
-  switch (op) {
-    case '+':
-    result=first + second;
-      printf("%.1lf + %.1lf = %.1lf", first, second, result );
-      break;
-    case '-':
-    result=first - second;
-      printf("%.1lf - %.1lf = %.1lf", first, second, result);
-      break;
-    case '*':
-    result=first * second;
-      printf("%.1lf * %.1lf = %.1lf", first, second,result );
-      break;
-    case '/':
-    result= first / second;
-      printf("%.1lf / %.1lf = %.1lf", first, second, result);
-      break;
-    
-    default:
-      printf("Error! operator is not correct");
+  switch (op)
+  {
+  case '+':
+    printf("%.10lf + %.10lf = %.10lf", first, second, first + second);
+    break;
+  case '-':
+    printf("%.10lf - %.10lf = %.10lf", first, second, first - second);
+    break;
+  case '*':
+    printf("%.10lf * %.10lf = %.10lf", first, second, first * second);
+    break;
+  case '/':
+    printf("%.10lf / %.10lf = %.10lf", first, second, first / second);
+    break;
+
+  default:
+    printf("Error! operator is not correct");
   }
 
   return 0;
